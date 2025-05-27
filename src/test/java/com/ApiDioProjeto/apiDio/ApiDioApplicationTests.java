53da176daf508bafc706833fc7544f848ce70be4
lucasleme102@gmail.com <lucasleme@Mac-mini-de-Lucas.local>
2025-05-27 15:36:11 -0300

    first commit

---
+++ src/test/java/com/ApiDioProjeto/apiDio/ApiDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApiDioProjeto.apiDio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
